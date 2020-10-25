e9d9ff84ad83a6b7cd2275e5a419cec58d937ccf
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-25 11:30:31 -0400

    added server.js db/connection.js, models dir, controllers dir, installed npm dependencies

---
+++ server.js
@@ -0,0 +1,30 @@
+// imports
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+// Server instance
+const app = express();
+
+// ENV Variables
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev')); //logging
+
+// Routes and Routers
+// Route for testing server is working
+app.get('/', (req, res) => {
+	res.json({
+		hello: 'Hello World! Take care of yourself today.',
+	});
+});
+
+//LISTENER
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
