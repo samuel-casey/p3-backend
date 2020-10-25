d2cfea8549ffe7801fbbe5311a08146034241695
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-25 12:38:12 -0400

    set up cors options and NODE_ENV in server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const corsOptions = require('./configs/cors');
-const { PORT, NODE_ENV } = process.env;
 require('dotenv').config();
 
 // Server instance
 const app = express();
 
 // ENV Variables
+const { PORT, NODE_ENV } = process.env;
 const PORT = process.env.PORT || 5000;
 
 // Middleware
