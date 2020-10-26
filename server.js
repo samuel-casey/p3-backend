ed7f6e552d58f2c75621b1cad57e6392e5280c9f
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-26 14:44:30 -0400

    enabled cors for all requests until post-mvp

--- server.js
+++ server.js
@@ -16,13 +16,17 @@ const { PORT, NODE_ENV } = process.env;
 // const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(NODE_ENV === 'production' ? cors(corsOptions) : cors());
+// LEAVING CORS OPTIONS FOR POST-MVP
+// app.use(NODE_ENV === 'production' ? cors(corsOptions) : cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev')); //logging
 
 // Routes and Routers
 // Route for testing server is working
+console.log('NODE_ENV', NODE_ENV);
+
 app.get('/', (req, res) => {
 	res.json({
 		hello: 'Hello World! Take care of yourself today.',
