4ac289da0915c428c9aa1c57d1705331ea4101ae
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-27 21:20:56 -0400

    Altered index route in wishlist to only show items for logged in user. Added try/catch blocks to all routes for error handling

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -3,26 +3,63 @@ const { Router } = require('express');
 const router = Router();
 const auth = require('../auth');
 
+// get all items for a given email
 router.get('/', auth, async (req, res) => {
-	res.json(await SelfCareItem.find({}));
+	try {
+		const { email } = req.payload;
+		const wishList = await SelfCareItem.find({ email: email });
+		res.status(200).json(wishList);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 });
 
+// create a new item
 router.post('/', auth, async (req, res) => {
-	res.json(await SelfCareItem.create(req.body));
+	try {
+		const newItem = await SelfCareItem.create(req.body);
+		res.status(200).json(newItem);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 });
 
+// get a single item given id
 router.get('/:id', auth, async (req, res) => {
-	res.json(await SelfCareItem.findById(req.params.id));
+	try {
+		const { id } = req.params;
+		const wishList = await SelfCareItem.find({ _id: id });
+		res.status(200).json(wishList);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 });
 
+// update a single item given id
 router.put('/:id', auth, async (req, res) => {
-	res.json(
-		await SelfCareItem.findByIdAndUpdate(req.params.id, req.body, { new: true })
-	);
+	try {
+		const { id } = req.params;
+		const { updates } = req.body;
+		console.log(updates);
+		const updatedItem = await SelfCareItem.findByIdAndUpdate(
+			id,
+			{ updates },
+			{ new: true }
+		);
+		res.status(200).json(updatedItem);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 });
 
+// delete a single item given id
 router.delete('/:id', auth, async (req, res) => {
-	res.json(await SelfCareItem.findByIdAndRemove(req.params.id));
+	try {
+		const deleted = await SelfCareItem.findByIdAndDelete(req.params.id);
+		res.status(200).json(deleted);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 });
 
 module.exports = router;
