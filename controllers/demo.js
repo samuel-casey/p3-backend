d71cfb59b951f12205ec637bc127770e91761681
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-29 10:31:32 -0500

    demo seed data

--- controllers/demo.js
+++ controllers/demo.js
@@ -3,6 +3,28 @@ const { Router } = require('express');
 const router = Router();
 const auth = require('../auth');
 
+
+const seedData = require('../db/demoSeed.json')
+router.post('/seed', auth, async (req, res) => {
+	try{		
+	const demoEmail = req.payload.email;
+	//loop over seedData
+	//find email key
+	//update email key with demo user email
+	// for(let item of seedData) {
+		for(let item =0; item < seedData.length; item++){
+		seedData[item].email = demoEmail
+		}
+		console.log('seedData', seedData)
+		console.log('length of seedData', seedData.length)
+		
+		const demoWishList = await SelfCareItem.create(seedData)
+	
+		res.status(200).json(demoWishList)
+	} catch (error) {
+		console.log(error)
+}})
+
 // create a new item for demo user
 router.post('/', auth, async (req, res) => {
 	try {
