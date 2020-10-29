53ae639f7526ff02e87d29d4c23e4bfe3f55ad2c
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-29 12:13:37 -0400

    pulled in dev origin

--- controllers/demo.js
+++ controllers/demo.js
@@ -3,25 +3,25 @@ const { Router } = require('express');
 const router = Router();
 const auth = require('../auth');
 
-
-const seedData = require('../db/demoSeed.json')
+const seedData = require('../db/demoSeed.json');
 router.post('/seed', auth, async (req, res) => {
-	try{		
-	const demoEmail = req.payload.email;
-	//loop over seedData
-	//find email key
-	//update email key with demo user email
-	// for(let item of seedData) {
-		for(let item =0; item < seedData.length; item++){
-		seedData[item].email = demoEmail
+	try {
+		const demoEmail = req.payload.email;
+		//loop over seedData
+		//find email key
+		//update email key with demo user email
+		// for(let item of seedData) {
+		for (let item = 0; item < seedData.length; item++) {
+			seedData[item].email = demoEmail;
 		}
-		
-		const demoWishList = await SelfCareItem.create(seedData)
-	
-		res.status(200).json(demoWishList)
+
+		const demoWishList = await SelfCareItem.create(seedData);
+
+		res.status(200).json(demoWishList);
 	} catch (error) {
-		console.log(error)
-}})
+		console.log(error);
+	}
+});
 
 // create a new item for demo user
 router.get('/', async (req, res) => {
